213f3d69326c39ea829628645c5d7246b354205f
Lintachaudhari <linta.chaudhari@omniwyse.com>
2019-06-28 09:54:23 +0530

    book list upload

--- src/app/book-list/book-list.component.ts
+++ src/app/book-list/book-list.component.ts
@@ -1,7 +1,10 @@
-import { Component, OnInit, ViewChild } from '@angular/core';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { BooksService } from './../services/books.service';
+import { Component, OnInit, ViewChild, ElementRef } from '@angular/core';
 import {MatPaginator} from '@angular/material/paginator';
 import {MatTableDataSource} from '@angular/material/table';
 import {MatSort} from '@angular/material/sort';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-book-list',
@@ -14,11 +17,43 @@ export class BookListComponent implements OnInit {
 
   displayedColumns: string[] = ['bookid', 'bookname', 'author', 'gnere', 'publisher', 'owner', 'action'];
   dataSource = new MatTableDataSource<PeriodicElement>(ELEMENT_DATA);
-
+  file: File = null;
   @ViewChild(MatPaginator) paginator: MatPaginator;
   @ViewChild(MatSort) sort: MatSort;
 
-  constructor() { }
+  constructor(private fb: FormBuilder, private bookservice: BooksService, private http: HttpClient) {}
+
+  // onFileChange(file: FileList) {
+  //   this.file = file.item(0);
+  // }
+
+  // onSubmit(uploadFile) {
+  //   this.bookservice.uploadfile(this.file).subscribe((data => {
+  //     console.log(data);
+  //   }));
+  // }
+  selectedFile: File = null;
+
+  onSelectFile(event) {
+    this.selectedFile = event.target.files[0];
+    console.log(this.selectedFile.name);
+  }
+
+
+  onUpload() {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': undefined,
+        Authorization: 'Basic ' + localStorage.getItem('Token'),
+        })
+      };
+    const fd = new FormData();
+    fd.append('file', this.selectedFile, this.selectedFile.name);
+    this.http.post('http://13.127.158.42/api/books/upload-books-file', fd, httpOptions). subscribe((data => {
+      console.log(data);
+    }));
+  }
+
 
   ngOnInit() {
     this.dataSource.paginator = this.paginator;
--- src/app/services/books.service.ts
+++ src/app/services/books.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, } from '@angular/common/http';
 import { HttpHeaders } from '@angular/common/http';
 import { Headers } from '@angular/http';
+import { AppSettings } from '../apiUrl';
 
 
 
@@ -18,4 +19,17 @@ export class BooksService {
     return this.http.post<AddBooks[]>(`http://13.127.158.42/admin/api/books`, books);
   }
 
+  uploadfile(file: any) {
+    const httpOptions = {
+      headers: new HttpHeaders({
+        'Content-Type': undefined,
+        Authorization: 'Basic ' + localStorage.getItem('Token'),
+        })
+      };
+    const formData: FormData = new FormData();
+
+    formData.append('file', file, file.name);
+
+    return this.http.post(AppSettings.URL + 'api/books/upload-books-file', formData, httpOptions);
+  }
 }
